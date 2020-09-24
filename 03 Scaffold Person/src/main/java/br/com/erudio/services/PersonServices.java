32fdebf20bb037ac5f9d88c3cbcb27cd28e4f1fc
mfmoreira <mfmoreira7@gmail.com>
2020-09-23 22:41:59 -0300

    Person entity with four verbs principals http request response

---
+++ 03 Scaffold Person/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,57 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+
+	public Person create(Person person) {
+		return person;
+	}
+
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Maria");
+		person.setLastName("Azevedo");
+		person.setAddress("São Gonçalo - Ceará - Brasil");
+		person.setGender("Feminine");
+		return person;
+	}
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Lastname" + i);
+		person.setAddress("Some address in Brazil" + i);
+		person.setGender("Feminine");
+		return person;
+	}
+
+}
