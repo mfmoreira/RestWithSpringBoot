9a1823d94923eb99a37d747401bc85915bdca0b3
mfmoreira <mfmoreira7@gmail.com>
2020-09-26 00:03:37 -0300

    Pattern VO with Dozer use and Junit Unitary Tests

---
+++ 05 Scaffold VO Pattern/src/main/java/br/com/odeveza/controller/PersonController.java
@@ -0,0 +1,52 @@
+package br.com.odeveza.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.odeveza.data.vo.PersonVO;
+import br.com.odeveza.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices services;
+
+	@GetMapping
+	public List<PersonVO> findAll() {
+		return services.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public PersonVO findById(@PathVariable("id") Long id) {
+		return services.findById(id);
+	}
+
+	@PostMapping
+	public PersonVO create(@RequestBody PersonVO personVO) {
+		return services.create(personVO);
+	}
+
+	@PutMapping
+	public PersonVO update(@RequestBody PersonVO personVO) {
+		return services.update(personVO);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		services.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
+}
