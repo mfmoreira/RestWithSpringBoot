35badf83f477811bb628d00c3f24c51d6f3528f5
mfmoreira <mfmoreira7@gmail.com>
2020-10-04 11:08:10 -0300

    Adding Swagger support

---
+++ 11 Scaffold Swagger/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'São Gonçalo - Ceará - Brasil','Maria','Female','Azevedo'),
+(3,'São Gonçalo - Ceará - Brasil','Caio','Male','Jorge'),
+(7,'São Gonçalo - Ceará - Brasil','Francisca','Female','Silva');
